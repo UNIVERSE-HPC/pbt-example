699bb8be51241f4567897f0e6c69abb8e8342209
Steve Crouch <s.crouch@software.ac.uk>
2024-04-18 11:31:06 +0100

    Update licence

--- encoding/rlencoding.py
+++ encoding/rlencoding.py
@@ -1,7 +1,12 @@
-def encode(input_string):
-    if not input_string:
-        return []
+# From: https://hypothesis.readthedocs.io/en/latest/quickstart.html
+# (C) 2013-2024, David R. MacIver. Available under Mozilla Public License v 2.0
+# https://github.com/HypothesisWorks/hypothesis/blob/master/LICENSE.txt
+#
+# Originally adapted from: https://rosettacode.org/wiki/Run-length_encoding
+# Under GNU Free Document License 1.3
+# https://www.gnu.org/licenses/fdl-1.3.en.html
 
+def encode(input_string):
     count = 1
     prev = ""
     lst = []
